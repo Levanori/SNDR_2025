61fd9b762df09baf5f1b9c22f0a18dd14fc9b934
Sereda Marina <seredamarinaplm7@gmail.com>
2025-04-16 21:07:06 +0300

    Created test_driver.cpp

---
+++ test_driver.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "calculate.h"
+#include "number_utils.h"
+#include "triangle.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "TestDriver" << endl;
+    return 0;
+}
